6e990677c0ac6740f46ddccfc51a29b609828bb7
bsettles <bsettles@localhost>
2014-02-04 23:10:41 -0600

    Removed some comments and helped with formatting.

--- Menu.py
+++ Menu.py
@@ -10,16 +10,11 @@ class Menu(pygame.Surface):
 
     def __init__(self, Player,xloc,yloc):
         
-        #pygame.init()
-
         self.WHITE = (255, 255, 255)
         self.GREEN = (0, 255, 0)
         self.BLUE = (0, 0, 255)
         self.BLACK = (0,0,0)
-
-        #pygame.font.init()
-        
-
+     
         self.Player = Player
         #X,Y coordinates of the surface itself
         self.xcoord = 0
@@ -33,8 +28,7 @@ class Menu(pygame.Surface):
         self.menusurface = pygame.Surface((500,300))
         self.menusurface.fill(self.BLACK)
         
-       
-        
+               
         fontObj = pygame.font.Font('freesansbold.ttf', 16) 
         
         iteminformationnumber = fontObj.render("No.", True, self.WHITE, self.BLACK)
@@ -42,9 +36,7 @@ class Menu(pygame.Surface):
         iteminformationdamage = fontObj.render("Damage", True, self.WHITE, self.BLACK)
         iteminformationhealth = fontObj.render("Health", True, self.WHITE, self.BLACK)
         iteminformationtype = fontObj.render("Type", True, self.WHITE, self.BLACK)
-        
-        
-        
+                    
         xcoord = 50
         ycoord = 30
         itemindex = 1
@@ -55,16 +47,14 @@ class Menu(pygame.Surface):
         self.menusurface.blit(iteminformationhealth, (xcoord+300, ycoord-20))
         self.menusurface.blit(iteminformationtype, (xcoord+400, ycoord-20))
         for item in self.Player.inventory.items:
-            #print(itemindex)
+          
             itemnumber = fontObj.render(str(itemindex), True, self.WHITE, self.BLACK)
             itemname = fontObj.render(str(item.name), True, self.WHITE, self.BLACK)
             itemdamage = fontObj.render(str(item.damage), True, self.WHITE, self.BLACK)
             itemhealth = fontObj.render(str(item.health), True, self.WHITE, self.BLACK)
             itemtype = fontObj.render(str(item.type), True, self.WHITE, self.BLACK)
             itemindex += 1
-            
-            
-            
+          
             self.menusurface.blit(itemnumber, (xcoord, ycoord)) #Consider making the xcoordinate addition for subsequent values a ratio of screen size versus hardcoded
             self.menusurface.blit(itemname, (xcoord+50, ycoord))
             self.menusurface.blit(itemdamage, (xcoord+200,ycoord))
@@ -73,8 +63,7 @@ class Menu(pygame.Surface):
             ycoord += 25
         return self.menusurface
         
-    #def newInventory(self):
-     
+   
     def Character(self):
         self.menusurface = pygame.Surface((480,310))
         self.menusurface.fill(self.BLACK)
@@ -103,7 +92,6 @@ class Menu(pygame.Surface):
         inventoryrender = fontObj.render(inventorystring, True, self.WHITE, self.BLACK)
         
         
-        
         self.menusurface.blit(namerender, (175, 20))
         self.menusurface.blit(levelrender, (170, 45))
         self.menusurface.blit(levelxprender, (170, 70))
@@ -111,8 +99,7 @@ class Menu(pygame.Surface):
         self.menusurface.blit(damagerender,(225, 150))
         self.menusurface.blit(goldrender,(15,200))
         self.menusurface.blit(inventoryrender,(225,200))
-        #print(self.Player.name)
-        
+     
         
         return self.menusurface
 
@@ -153,7 +140,6 @@ class InventoryMenu(Menu):
     tempy=60
    else:
     tempx=tempx+50
-
     
 class EquipmentMenu(Menu):
 
@@ -201,8 +187,6 @@ class EquipmentMenu(Menu):
     self.menusurface.blit(item.image,(self.xcoord+10,self.ycoord+160))
    if item.type=='shield':
     self.menusurface.blit(item.image,(self.xcoord+150,self.ycoord+160))
-
-        
         
     def Equipment(self):
         self.menusurface = pygame.Surface((500,300))
@@ -241,37 +225,5 @@ class EquipmentMenu(Menu):
             self.menusurface.blit(itemtype, (xcoord+400,ycoord))
             ycoord += 25
 
-            
- 
-
-        #print(self.Player.name)
-        
-        
         return self.menusurface
         
-#WHITE = (255, 255, 255)
-
-#pygame.init()
-#DISPLAYSURF = pygame.display.set_mode((500,500), 0, 32)
-#pygame.display.set_caption('Dev') #Window Title
-
-#DISPLAYSURF.fill(WHITE) #Fill the screen area with white.
-
-#player = Player(32,32,'images/player.png',"David",100,10)
-
-#item1 = Item(0, "sword", 10, 10,'images/player.png',"1h")
-#item2 = Item(1, "monkey dick", 5, 35,'images/player.png',"2h")
-#item3 = Item(32, "monkey dick", 5, 35,'images/player.png',"2h")
-#item4 = Item(5, "grawblewarble", 10, 15, 'images/player.png', "1h")
-
-#player.inventory.add_item(item1)
-#player.inventory.add_item(item2)
-#player.inventory.add_item(item3)
-#player.inventory.add_item(item4)
-
-
-#menu = Menu(player)
-#menu.Character()
-
-#DISPLAYSURF.blit(menu.Character(), (25, 25))
-#pygame.display.update()
